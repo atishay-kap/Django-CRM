6a891f0b5b15f73900177d74472f2ccca1884913
Atishay Kapoor <atishaykapoor26@gmail.com>
2025-06-23 17:42:27 +0530

    Final Project

--- website/views.py
+++ website/views.py
@@ -54,3 +54,53 @@ def register_user(request):  # sourcery skip: extract-method
     
     return render(request , 'register.html' , {'form' : form})
 
+
+def customer_record(request, pk):
+    if request.user.is_authenticated:
+        customer_record = Record.objects.get(id=pk)
+        return render(request , 'record.html' , {'customer_record' : customer_record})
+    else:
+        messages.success(request , 'You must be logged in to view that page...')
+        return redirect('home')
+    
+def delete_record(request, pk):
+    if request.user.is_authenticated:
+        delete_it = Record.objects.get(id=pk)
+        delete_it.delete()
+        messages.success(request , f"The Customer with ID : {pk} is deleted successfully")
+        return redirect('home')
+    else:
+        messages.success(request , 'You must be logged in to view that page...')
+        return redirect('home')
+    
+    
+def add_record(request):
+    
+    form = AddRecordForm(request.POST or None)
+    
+    if request.user.is_authenticated :
+        if request.method == "POST":
+            if form.is_valid():
+                add_record = form.save()
+                messages.success(request , "Record Added Successfully...")
+                return redirect('home')
+        return render(request , 'add_record.html' , {'form' : form})
+    else:
+        messages.success(request , "You need to login to add records!!")
+        return redirect('home')
+    
+def update_record(request , pk):
+    if request.user.is_authenticated:
+        current_record = Record.objects.get(id=pk)
+        form = AddRecordForm(request.POST or None, instance=current_record)
+        if form.is_valid():
+            form.save()
+            messages.success(request , "Record has been updated successfully...")
+            return redirect('home')
+        return render(request , 'update_record.html' , {'form' : form})
+    else:
+        messages.success(request , "You need to login to update records!!")
+        return redirect('home')
+        
+            
+        
